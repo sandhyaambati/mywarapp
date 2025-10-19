55c58cce99ac97e3ff23473316d435fcba6f8851
sandhyaambati <ambatisandhya76@gmail.com>
2025-10-19 14:54:13 -0400

    create new repo

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Welcome to My WAR Project!</h1>");
+        out.println("<a href='index.jsp'>Go to UI Page</a>");
+        out.println("</body></html>");
+    }
+}
