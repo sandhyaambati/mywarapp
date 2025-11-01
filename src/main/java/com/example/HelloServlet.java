52d25d82926a2472bffbf967d92d827a56b4bc62
sandhyaambati <ambatisandhya76@gmail.com>
2025-11-01 09:13:07 -0400

    Update HelloServlet.java

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -1,6 +1,6 @@
 package com.example;
 
-import jakarta.servlet.ServletException;
+//import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -43,3 +43,4 @@ public class HelloServlet extends HttpServlet {
         }
     }
 }
+
